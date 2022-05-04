e2236f96e2851bab490805b98550254e9e95a37a
apaulcloud <104290739+apaulcloud@users.noreply.github.com>
2022-05-03 21:41:03 -0400

    Initial commit

--- IIQF1.py
+++ IIQF1.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# IIQF - first Class by Dr. Hari
-# 
+# IIQF - IntelliJ project
 
-# What is
-# - IID : Independent and identically distributed
-# - Git : Git is a DevOps tool used for source code management. It is a free and open-source version control system used to handle small to very large projects efficiently. Git is used to tracking changes in the source code, enabling multiple developers to work together on non-linear development.
-# - Git and Git desktop 
-# - install Anaconda
-# - complete Python recording
-# - Excel
-# - Explore Jupyter notebook
-# - Create Git account
-# - in GIT, create one repository
-# - Jupyter notebook 
-#   - markdown mode
-#   - write 10 lines from your resume and get it as a text
-#   
 # - install IntelliJ and create first package
-# 
-# 
+#
 
 # - esc + a
 # - esc +b
@@ -28,27 +12,19 @@
 # - Ctrl + enter
 # - esc d
 # - esc + m
-# 
-
-# 
-
-# In[ ]:
 
 
 # first python program in IntelliJ Idea
 
 print("Amit is a professional with more than eighteen years of experience in " + "\n"
-                                                                                 "implementing financial markets and infrastructure projects")
+             "implementing financial markets and infrastructure projects")
 
 str1 = "hello ... 3rd line " + "starts "
 
-# In[ ]:
+# convert to upper case
+print("UpperCase"+str1.upper() + "\n")
 
-print(str1.upper() + "\n")
-
-print(str1.title())
-
-# In[ ]:
+print("First character in upper case"+str1.title())
 
 
 n1 = 23
@@ -58,94 +34,41 @@ print(n1)
 
 # # home work May 7th
 # basics of intellij
-# - check how get plugins for putho
+# - check how get plugins for python
 # - create python project
-# - share the link of recorded sesson
-# - create pme pythn projct with intellij
-# - take jupyter notebook codde and write to one .py file and create and run 
-# 
+# - share the link of recorded session
+# - create a python project with intellij
+# - take jupyter notebook code and write to one .py file and create and run
 # - how to see plotted graph in IntelliJ
 
-# # homework, 1 week, Apr 30th
-# abhishek, nivththa, kayastha
-# Ajay, 
-# 
-# - 20-30 min recorded
-# - GUI git desktop, install it
-# - create github acc
-
-# # home work - everyone
-# 5 min
-# - Ajay, Abhishek kayastha
-# - read stock market data with pandas from csv
-# - read stock data from some website
-# 
-# -read data from website, yahoo finance
-# import pandas_datareader as web
-# #! pip install pandas_datareader
-# web.DataReader("HDFC.NS", data_source = "yahoo")
-# 
-# - nse 
-# - use web.DataReader("HDFC.NS",:"yhaoo ..")
-# web.DataReader("HDFC.NS", data_source = "yahoo")
-# iloc
-# df.head(10)
-# 
-# 
-# help (DataReader)
-# 
-
-# # home work one month
-# - Abhishek
-# - numpy - generate random numbers
-# - normal distribution
-# - uniform ddistribution
-# - add some image to explain what is normal Gausian dist
-# - what is the difference betwee normal an uniform dist ?
-# 
-# import numberp as np
-# np.random.rand()
-
-# # home work 5
-# - read and write data
-# 
-# - read data from website for diff stocks
-# - write data to database, mysql, develop code in jupyter notebook
-# - use diff query for this data
-# - count, mean
-# 
-
 import pandas as pd
-
-print("imported pandas")
-# !pip install pandas
-
-
-# ! pip install pandas_datareader
-
 import matplotlib.pyplot as plt
 import pandas_datareader as web
-import pandas_datareader.data as pd_data
+import pandas_datareader.data as pd_data #not needed
 
+# !pip install pandas
+# ! pip install pandas_datareader
 # !pip install yfinance
 
+print("imported pandas and matplotlib")
 stk = web.DataReader("HDFC.NS", data_source="yahoo")
 print(stk)
-print("hdfc.ns imported from yahoo")
+print("hdfc.ns imported from yahoo...plotting graph")
 
-stk['Close'].plot()
+stk['Close'].plot() #close price only
 plt.show()
 
 print("hdfc plotted")
 
+#Plot only for a specific Date range
 from datetime import datetime
-
 start = datetime(2022, 1, 1)
-
 end = datetime(2022, 3, 31)
 
-
 TD_DataFrame = web.DataReader('TD', 'yahoo', start, end)
+print("start plotting TD")
+TD_DataFrame['Close'].plot()
+plt.show()
 
 # Define the ticker list
 # import pandas as pd
@@ -153,49 +76,29 @@ tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP']
 
 # Fetch the data
 import yfinance as yf
-yf_data = yf.download(tickers_list,'2015-1-1')['Adj Close']
+
+data = yf.download(tickers_list, '2015-1-1')['Adj Close']
 
 # Print first 5 rows of the data
-# print(data.head())
+print(data.head())
 
 
 # conda info --envs
 
-data_csv = pd.read_csv('Data/AAPL.csv')
+data_csv = pd.read_csv('Data/AAPL.csv') #file should be in exact location
 
 data_csv.describe()
 
-# In[23]:
-
-
 data_csv.plot()
-
-# In[35]:
-
 
 data_csv.columns
 
-# In[28]:
-
-
 data_csv['Open'].plot()
-
-# In[29]:
-
 
 data_csv['Close'].plot()
 
-# In[37]:
-
-
 data_csv.info()
 
-# In[39]:
-
-
 data_csv.info
-
-# In[25]:
-
 
 data_csv.dtypes
