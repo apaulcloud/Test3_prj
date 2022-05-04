91dd9da68234db377a2e996cb663fd39425d0046
apaulcloud <104290739+apaulcloud@users.noreply.github.com>
2022-05-03 22:02:41 -0400

    am1

--- IIQF1.py
+++ IIQF1.py
@@ -153,7 +153,7 @@ tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP']
 
 # Fetch the data
 import yfinance as yf
-yf_data = yf.download(tickers_list,'2015-1-1')['Adj Close']
+data = yf.download(tickers_list,'2015-1-1')['Adj Close']
 
 # Print first 5 rows of the data
 # print(data.head())
